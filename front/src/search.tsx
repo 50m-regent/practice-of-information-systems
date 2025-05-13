762b06e5f96d17eeea55a2961eb5d7bb5e0799bf
somacoro <coron.214.soma@gmail.com>
2025-05-13 09:42:06 +0900

    検索機能(ダミーデータ利用/apiはコメントアウト中)

--- front/src/search.tsx
+++ front/src/search.tsx
@@ -1,21 +1,93 @@
 import { LinkButton } from "./components/test/link";
 import { Navbar } from './components/Navbar';
+import { useState, useEffect, useCallback} from "react";
+import axios from "axios";
+import debounce from "lodash/debounce";
 
-export const Search = () => {
+type DysplayMusic ={
+    id : number;
+    title : string;
+    artist : string;
+    thumbnail : string;//画像のURL? 画像そのもの？
+}
+export const Search = () => {//検索画面と結果表示画面をもつ
     const title: string = "検索画面";
+    const [searchQuery, setSearchQuery] = useState<any>("");
+    const [musicList, setMusicList] = useState<DysplayMusic[]>([]);
+
+    const fetchMusic = useCallback(//検索クエリを元に音楽リストを取得する関数
+        // debounceを使用して、入力が止まってから500ms後に実行
+        // useCallbackを使用して、関数をメモ化?
+        debounce(async (query: string) => {
+            // console.log(musicList);
+            if (!query) {
+                setMusicList([]);
+                return;
+            }
+            try {
+                // const response = await axios.get(`http://localhost:8080/search?${query}`);
+                // setMusicList(response.data);
+                // ここではダミーデータを使用
+                // 変更するたびに要素が追加されるダミー
+                const test = "test"+query;
+                setMusicList(prevList => {
+                    const updated = [{ id: musicList.length, title: test, artist: test, thumbnail: test }, ...prevList];
+                    console.log("検索結果:", updated);
+                    return updated;
+                });
+            } catch (error) {
+                console.error("検索に失敗:", error);
+            }
+        }, 500),
+        [] // debounceは一度だけ定義
+      );
+      
+      useEffect(() => {// 検索クエリが変更されたときにfetchMusicを呼び出す
+        fetchMusic(searchQuery);
+      }, [searchQuery, fetchMusic]);
 
     return (
         <>
         <div className="Search">
-            <h1>{title}</h1>
-            <LinkButton text="ホーム画面/Home" link="/home" />
-            <LinkButton text="検索結果画面/Result" link="/result" />
-            <h3>ヘッダー</h3>
-            <LinkButton text="Spotify認証/Auth" link="/auth" />
-            <h3>フッター</h3>
-            <LinkButton text="ホーム画面/Home" link="/home" />
-            <LinkButton text="検索画面/Search" link="/search" />
-            <LinkButton text="お気に入り画面/Favorite" link="/favorite" />
+            <div>
+            <input type="search" 
+                size="30" 
+                placeholder="曲名・アーティスト名で検索" 
+                // value={searchQuery}
+                onChange={(event) => {
+                    if (event.target.value === "") {
+                        setSearchQuery("");
+                    }else{
+                        setSearchQuery("title="+event.target.value);
+                    }
+                }} //アーティスト・楽曲名検索クエリ
+                />
+            </div>
+            
+            {
+                (musicList.length > 0) ? (//検索クエリが存在している場合の画面
+                    <div>
+                        {musicList.map((music, index) => (
+                            // 音楽表示用コンポーネントの作成
+                        <p key={index}>{music.title}</p>
+                        ))}
+                    </div>
+                 ) : (//検索クエリが存在しない場合の画面
+                    <div>
+                    <p>直近の検索~未実装~</p>
+                        <div>
+                        {[...Array(9)].map((_, i) => {
+                            const level = i + 1;
+                            return (
+                            <button key={level} onClick={() => setSearchQuery(`difficulty=${level}`)}>
+                                難易度{level}
+                            </button>
+                            );
+                        })}
+                        </div>
+                    </div>
+                )
+            }
         </div>
         <Navbar />
         </>
